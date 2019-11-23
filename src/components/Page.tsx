4d56cbe2044c549c2c97620f4c8d4a04fdf1d212
Roman Naumenko <rnaumenko@s-pro.io>
2019-11-23 09:20:32 +0200

    update types

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -1,7 +1,11 @@
-import styles from "./style.module.scss"
-import { IPage } from "./types"
+import * as React from 'react'
+import styles from './style.module.scss'
+import { IPage } from './types'
 
-export default function Page({ title, children }: IPage) {
+export default function Page({
+  title,
+  children,
+}: IPage): React.ReactElement<IPage> {
   return (
     <div className={styles.page}>
       <h1>{title}</h1>
